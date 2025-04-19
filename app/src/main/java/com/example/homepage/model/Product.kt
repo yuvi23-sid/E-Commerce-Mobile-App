a1ad5045c2fcf245fd19c311c256d014577bfaca
Felipe Mora <felipe.moraa13@gmail.com>
2025-04-18 20:28:11 -0600

    We are cleaning the old folder files
    We are removing the old files to upload the new ones in one upload due to a version of Andorid

--- app/src/main/java/com/example/homepage/model/Product.kt
+++
@@ -1,8 +0,0 @@
-package com.example.homepage.model
-
-data class Product(
-    val id: Int,
-    val title: String,
-    val price: Float,
-    val image: String
-)
--- app/src/main/java/com/example/homepage/network/FakeStoreApi.kt
+++
@@ -1,9 +0,0 @@
-package com.example.homepage.network
-
-import com.example.homepage.model.Product
-import retrofit2.http.GET
-
-interface FakeStoreApi {
-    @GET("products")
-    suspend fun getProducts(): List<Product>
-}
